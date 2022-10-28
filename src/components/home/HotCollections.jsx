578ec13a23e7cf3b7f310e81b6bed2e571402118
codeSB10 <susan.baral1708@gmail.com>
2022-10-29 00:28:13 +0545

    Modified a code

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import OwlCarousel from "react-owl-carousel";
 import Skeleton from "../UI/Skeleton";
